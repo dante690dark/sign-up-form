0239ac31d04d8b8946b53a2e91e370a6f77666e7
Alexander <dante690dark@gmail.com>
2025-02-18 23:02:09 -0500

    add js to validate regular expression

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const password = document.getElementById("password");
+  const confirmPassword = document.getElementById("confirm_password");
+  const form = document.querySelector("form");
+
+  const validPassword = () => {
+    confirmPassword.style.border = "2px solid green";
+      confirmPassword.style.backgroundColor = "rgba(40, 167, 69, 0.1)";
+  }
+
+  const invalidPassword = () => {
+    confirmPassword.style.border = "2px solid red";
+    confirmPassword.style.backgroundColor = "rgba(255, 0, 0, 0.1)";
+  }
+
+  const validatePasswords = () => {
+
+    if (confirmPassword.value === "") return;
+  
+    confirmPassword.value !== password.value ? invalidPassword() : validPassword();
+  }
+
+  password.addEventListener("input", validatePasswords);
+  confirmPassword.addEventListener("focus", validatePasswords);
+  confirmPassword.addEventListener("input", validatePasswords);
+
+  form.addEventListener("submit",  (event) => {
+    event.preventDefault()
+    validatePasswords();
+  });
+});
